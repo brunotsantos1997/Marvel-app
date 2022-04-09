780b297022a40de1f9c5324eb5f39c4a1a1d9b22
theKingWolf <bruno.t.santos1997@hotmail.com>
2022-04-09 13:32:12 -0300

    :sparkles: inplemente Module

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/di/Modele.kt
@@ -0,0 +1,40 @@
+package daniel.lop.io.marvelappstarter.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import daniel.lop.io.marvelappstarter.data.remote.ServiceApi
+import daniel.lop.io.marvelappstarter.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Modele {
+    @Singleton
+    @Provides
+    fun provideOkttpClient(): OkHttpClient {
+        return OkHttpClient()
+    }
+
+    @Singleton
+    @Provides
+    fun porvideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi {
+        return retrofit.create(ServiceApi::class.java)
+    }
+
+}
