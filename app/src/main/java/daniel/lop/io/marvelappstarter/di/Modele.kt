aecd3d99670fd4f42930f1ba241657f2f00a5427
theKingWolf <bruno.t.santos1997@hotmail.com>
2022-04-10 17:15:18 -0300

    :sparkles: introduce okHttpClient

--- app/src/main/java/daniel/lop/io/marvelappstarter/di/Modele.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/di/Modele.kt
@@ -5,25 +5,55 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import daniel.lop.io.marvelappstarter.data.remote.ServiceApi
+import daniel.lop.io.marvelappstarter.util.Constants
 import daniel.lop.io.marvelappstarter.util.Constants.BASE_URL
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import timber.log.Timber
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object Modele {
+object Module {
     @Singleton
     @Provides
-    fun provideOkttpClient(): OkHttpClient {
-        return OkHttpClient()
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient().newBuilder()
+            .addInterceptor { chain ->
+                val currentTimeStamp = System.currentTimeMillis()
+                val newUrl = chain.request().url
+                    .newBuilder()
+                    .addQueryParameter(Constants.TS, currentTimeStamp.toString())
+                    .addQueryParameter(Constants.APIKEY, Constants.PUBLIC_KEY)
+                    .addQueryParameter(
+                        Constants.HASH,
+                        provideToMd5Hash(
+                            currentTimeStamp.toString() +
+                                    Constants.PRIVATE_KEY + Constants.PUBLIC_KEY
+                        )
+                    )
+                    .build()
+                val newRequest = chain.request()
+                    .newBuilder()
+                    .url(newUrl)
+                    .build()
+                chain.proceed(newRequest)
+            }
+            .addInterceptor(logging)
+            .build()
     }
 
     @Singleton
     @Provides
-    fun porvideRetrofit(client: OkHttpClient): Retrofit {
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -37,4 +67,26 @@ object Modele {
         return retrofit.create(ServiceApi::class.java)
     }
 
+    @Singleton
+    @Provides
+    fun provideToMd5Hash(encrypted: String): String {
+        var pass = encrypted
+        var encryptedString: String? = null
+
+        val md5: MessageDigest
+        try {
+            md5 = MessageDigest.getInstance("MD5")
+            md5.update(pass.toByteArray(), 0, pass.length)
+            pass = BigInteger(1, md5.digest()).toString(16)
+            while (pass.length < 32) {
+                pass = "0$pass"
+            }
+            encryptedString = pass
+        } catch (e1: NoSuchAlgorithmException) {
+            e1.printStackTrace()
+        }
+        Timber.d("hash -> $encryptedString")
+        return encryptedString ?: ""
+
+    }
 }
--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -4,4 +4,8 @@ object Constants {
     const val BASE_URL = "https://gateway.marvel.com/V1/public/"
     const val PUBLIC_KEY = "14282c4e378898fc99167af9f54a6ac5"
     const val PRIVATE_KEY = "ab900f64b68e85760983dc034d19b0819c1a6ec1"
+
+    const val APIKEY = "APIKEY"
+    const val HASH = "hash"
+    const val TS = "ts"
 }
