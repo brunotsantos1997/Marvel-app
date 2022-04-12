8bb4255205b4cadbb8a26bb102d1ad5f3ec789f8
theKingWolf <bruno.t.santos1997@hotmail.com>
2022-04-12 15:56:42 -0300

    :recycle: refactor character fragment

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/list/ListCharacterFragment.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/list/ListCharacterFragment.kt
@@ -1,15 +1,82 @@
 package daniel.lop.io.marvelappstarter.ui.list
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModel
 import daniel.lop.io.marvelappstarter.databinding.FragmentListCharacterBinding
+import daniel.lop.io.marvelappstarter.ui.adapters.CharacterAdapter
 import daniel.lop.io.marvelappstarter.ui.base.BaseFragment
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import daniel.lop.io.marvelappstarter.util.hide
+import daniel.lop.io.marvelappstarter.util.show
+import daniel.lop.io.marvelappstarter.util.toast
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCharacterViewModel>() {
     override val viewModel: ListCharacterViewModel by viewModels()
+
+    private val characterAdapter by lazy { CharacterAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecycleView()
+        clickAdapter()
+        collectObserver()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.list.collect{ resource ->
+            when(resource){
+                is ResourceState.Sucess->{
+                    binding.progressCircular.hide()
+                    resource.data?.let { values ->
+                        characterAdapter.characters = values.data.results.toList()
+                    }
+                }
+                is ResourceState.Error->{
+                    binding.progressCircular.hide()
+                    resource.message?.let { message ->
+                        toast(getString(R.string.an_error_occurred))
+                        Timber.tag("ListCharacterFragment").e("error -> $message")
+                    }
+                }
+                is ResourceState.Loading->{
+                    binding.progressCircular.show()
+
+                }
+                else->{
+                }
+            }
+
+        }
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnClickListner { characterModel ->
+            val action = ListCharacterFragmentDirections
+                .actionListCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupRecycleView() = with(binding) {
+        rvCharacters.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 
     override fun getViewBiding(
         inflater: LayoutInflater,
