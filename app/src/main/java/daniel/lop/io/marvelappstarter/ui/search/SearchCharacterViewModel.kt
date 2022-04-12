7a59f3534da53936408f875248517aba62de265b
theKingWolf <bruno.t.santos1997@hotmail.com>
2022-04-12 15:57:44 -0300

    :recycle: refactor search character and implement view model

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/search/SearchCharacterViewModel.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/search/SearchCharacterViewModel.kt
@@ -1,6 +1,46 @@
 package daniel.lop.io.marvelappstarter.ui.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModelResponse
+import daniel.lop.io.marvelappstarter.repository.MarvelRepository
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class SearchCharacterViewModel : ViewModel() {
+class SearchCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+): ViewModel() {
+    private val _searchCharacter = MutableStateFlow<ResourceState<CharacterModelResponse>>(ResourceState.Empty())
+    val searchCharacter: StateFlow<ResourceState<CharacterModelResponse>> = _searchCharacter
+
+    fun fetch(nameStartsWith: String) = viewModelScope.launch {
+        safeFetch(nameStartsWith)
+    }
+
+    private suspend fun safeFetch(nameStartsWith: String) {
+        _searchCharacter.value = ResourceState.Loading()
+        try {
+            val response = repository.list(nameStartsWith)
+            _searchCharacter.value = handleResponse(response)
+        }catch (t: Throwable){
+            when(t){
+                is IOException -> _searchCharacter.value = ResourceState.Error("Erro de conexão com a internet")
+                else -> _searchCharacter.value = ResourceState.Error("Falha na conversão de dados")
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<CharacterModelResponse>): ResourceState<CharacterModelResponse> {
+        if(response.isSuccessful){
+            response.body()?.let { values ->
+                return ResourceState.Sucess(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
 }
--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -1,9 +1,12 @@
 package daniel.lop.io.marvelappstarter.util
 
 object Constants {
-    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
     const val PUBLIC_KEY = "14282c4e378898fc99167af9f54a6ac5"
     const val PRIVATE_KEY = "ab900f64b68e85760983dc034d19b0819c1a6ec1"
+
+    const val LAST_SEARCH_QUERY= "last_search_query"
+    const val DEFAULT_QUERY = ""
 
     const val APIKEY = "APIKEY"
     const val HASH = "hash"
